a5ac8a5ac8cd2f004b5f6db9bce6c7eb63bdcb42
manel <toutouhelwa@gmail.com>
2026-09-23 20:23:24 +0200

    Remove unused route imports in server.js for cleaner code

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -13,9 +13,8 @@ import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
 import clerkWebhook from "./webhooks/clerk.webhook.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+
+
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -33,8 +32,7 @@ app.get("/health", (req, res) => {
     res.status(200).json({ ok: true });
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+
 
 // if the public directory exists, serve the static files
 // this is for the production build
