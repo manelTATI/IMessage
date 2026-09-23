f2a95679e4c7d7dadc53d59d6df8ccda26559994
manel <toutouhelwa@gmail.com>
2026-09-23 20:13:29 +0200

    Fix indentation for improved code readability in server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -30,7 +30,7 @@ app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ ok: true });
+    res.status(200).json({ ok: true });
 });
 
 app.use("/api/auth", authRoutes);
@@ -39,16 +39,15 @@ app.use("/api/messages", messageRoutes);
 // if the public directory exists, serve the static files
 // this is for the production build
 if (fs.existsSync(publicDir)) {
-  app.use(express.static(publicDir));
+    app.use(express.static(publicDir));
 
-  app.get("/{*any}", (req, res, next) => {
-    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
-  });
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
 }
 
 server.listen(PORT, () => {
-  connectDB();
-  console.log("Server is up and running on PORT:", PORT);
-
-  if (process.env.NODE_ENV === "production") job.start();
+    connectDB();
+    console.log("Server is up and running on PORT:", PORT);
+    if (process.env.NODE_ENV === "production") job.start();
 });
