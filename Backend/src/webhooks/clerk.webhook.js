cb5fbb823831b7384767dcc7929da64795a36a5d
manel <toutouhelwa@gmail.com>
2026-09-24 16:26:54 +0200

    Add GET endpoint to clerk webhook for route verification

--- Backend/src/webhooks/clerk.webhook.js
+++ Backend/src/webhooks/clerk.webhook.js
@@ -3,6 +3,9 @@ import User from "../models/user.model.js";
 import { verifyWebhook } from "@clerk/backend/webhooks";
 
 const router = express.Router();
+router.get("/", (req, res) => {
+    res.send("Clerk webhook route is working");
+});
 
 router.post("/", async (req, res) => {
     try {
